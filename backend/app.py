c336f0f48bf866bc745684c15ae032dde6536119
Mansour-Gaye <mgaye2118@gmail.com>
2025-06-11 16:32:40 +0000

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from whatsapp_webhook import whatsapp
 
@@ -5,4 +6,5 @@ app = Flask(__name__)
 app.register_blueprint(whatsapp, url_prefix='/whatsapp')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # 🔁 important pour Render
+    app.run(debug=False, host="0.0.0.0", port=port)
